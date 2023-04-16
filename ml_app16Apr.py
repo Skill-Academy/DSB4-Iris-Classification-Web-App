7c426a905b450d970a29d38b67c1c87c468b04b3
Skill-Academy <121305504+Skill-Academy@users.noreply.github.com>
2023-04-16 11:39:55 +0530

    Add files via upload

---
+++ ml_app16Apr.py
@@ -0,0 +1,38 @@
+
+import pickle
+import streamlit as st
+
+
+rf = pickle.load(open('rf_model.pkl','rb'))
+lr = pickle.load(open('lr_model.pkl','rb'))
+knn = pickle.load(open('knn_model.pkl','rb'))
+
+st.title('Iris Web App')
+
+ml_model = ['Logistic Regression','RandomForest Classiifer','KNN Classifier']
+
+option = st.sidebar.selectbox('Select the ML model',ml_model)
+
+sl = st.slider('Select Sepal Length', 0.0,10.0)
+sw = st.slider('Select Sepal Width', 0.0,10.0)
+pl = st.slider('Select Petal Length', 0.0,10.0)
+pw = st.slider('Select Petal Width', 0.0,10.0)
+
+test =  [[sl,sw,pl,pw]]
+
+st.write('Test Data')
+st.write(test)
+
+
+if st.button('Classify'):
+    res = None
+    if option=='Logistic Regression':
+        st.success(lr.predict(test)[0])
+    elif option=="RandomForest Classiifer":
+        st.success(rf.predict(test)[0])
+    else:
+        st.success(knn.predict(test)[0])
+
+# st.success(res)
+
+
